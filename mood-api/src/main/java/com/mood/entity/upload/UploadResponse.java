cda9c2c86e6c56a65a80e4e94ce30daa8297e4c1
李金璐 <lijinlu@lianyu.io>
2018-08-08 23:06:54 +0800

    Initial commit

---
+++ mood-api/src/main/java/com/mood/entity/upload/UploadResponse.java
@@ -0,0 +1,19 @@
+package com.mood.entity.upload;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.mood.entity.base.PagerResponse;
+import lombok.Data;
+
+/**
+ * 应用模块
+ * @author fenglu
+ * @time 2018-06-08 16:08
+ */
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer", "handler", "fieldHandler"}, ignoreUnknown = true)
+public class UploadResponse extends PagerResponse {
+
+    private String url;
+}
